331f3b36bc9d15c40ba428eb786b3cabff0a73cf
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-11-28 21:06:37 -0500

    save work for pull

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,13 +15,13 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            LITFLIX
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
             <Nav className='ml-auto d-flex'>
               <Nav.Link as={Link} to='/'>
-                Search For Books
+                Title Search
               </Nav.Link>
               {/* if user is logged in show saved books and logout */}
               {Auth.loggedIn() ? (
